62fd33ebf694b93bf4d970d37aadfb7b9e46056c
Landon Bouma <landonb@users.noreply.github.com>
2024-07-12 19:50:17 -0500

    Update: UX: On macOS, don't emit newline after empty response

--- bin/ask-yesnoskip.sh
+++ bin/ask-yesnoskip.sh
@@ -42,7 +42,10 @@ ask_yesnoskip () {
   else
     read the_choice
 
-    if [ -z "${the_choice}" ]; then
+    # On Linux (Bash v5), hitting Enter at the prompt doesn't print
+    # a newline, so do so here. But on macOS (whether Bash v3 or
+    # Bash v5) read prints the newline, so don't do so here.
+    if [ -z "${the_choice}" ] && ! os_is_macos; then
       >&2 echo
     fi
   fi
@@ -68,6 +71,12 @@ ask_yesnoskip () {
 
   # Prints either 'y' or 's'.
   printf %s "${the_choice}"
+}
+
+# ***
+
+os_is_macos () {
+  [ "$(uname)" = 'Darwin' ]
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
